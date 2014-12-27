55ea9caddfeefcadf7a76233fd190ec4c0acda66
krashpanic <krashpanic@gmail.com>
2014-12-27 09:18:52 -0800

    hello1.go

---
+++ hello1.go
@@ -0,0 +1,19 @@
+//Test
+
+package main
+
+import "fmt"
+
+/* Testing 123
+
+testing 456
+*/
+func main() {
+	var radius float32
+	fmt.Scanf("%f", &radius)
+
+	var length float32
+	length = 2 * 3.14 * radius
+
+	fmt.Println("Length:", length)
+}
